a8368831ac5753867f397f51a23d3186f6c1504f
Dibyaranjan27 <dibyam2427@gmail.com>
2026-08-17 23:14:20 +0530

    Add bmi_calculator_flutter project and updated README

---
+++ bmi_calculator_flutter/lib/components/round_icon_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RoundedIconButton extends StatelessWidget {
+  const RoundedIconButton(
+      {super.key, required this.icon, required this.onPress});
+
+  final IconData icon;
+  final void Function() onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: onPress,
+      constraints: const BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      shape: const CircleBorder(),
+      elevation: 0.0,
+      fillColor: const Color(0xFF4C4F5E),
+      child: Icon(icon),
+    );
+  }
+}
